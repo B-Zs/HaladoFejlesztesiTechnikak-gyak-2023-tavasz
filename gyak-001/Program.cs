7926ac5039769532adc9a7066a974eb0b0ec2259
Dr. Ádám Csapó <csapo.adam@gmail.com>
2023-03-01 20:39:52 +0100

    majdnem jo, de mi a problema megis?

--- gyak-001/Program.cs
+++ gyak-001/Program.cs
@@ -12,8 +12,73 @@ using System.Threading.Tasks;
 
 namespace _2023_02_28_Events
 {
+    enum Category
+    {
+        OPINION,
+        BUGREPORT,
+        FEATUREREQ
+    }
+
+    internal class Feedback
+    {
+        public Feedback(Category category, string description)
+        {
+            this.category = category;
+            this.description = description;
+        }
+
+        public Category category { get; }
+        public string description { get; }
+    }
+
+    internal class FeedbackProcessor
+    {
+        private Dictionary<Category, Action<Feedback>> Actions;
+        private List<Feedback> Feedbacks;
+
+        public FeedbackProcessor(Action<Feedback> defaultAction)
+        {
+            Console.WriteLine("fb processor created");
+            Actions = new Dictionary<Category, Action<Feedback>>();
+            foreach (Category cat in Enum.GetValues(typeof(Category)))
+            {
+                Actions[cat] = defaultAction;
+            }
+            Feedbacks = new List<Feedback>();
+        }
+
+        public void AddFeedback(Feedback fb)
+        {
+            Feedbacks.Add(fb);
+            Console.WriteLine("Feedback added" + Feedbacks.Count);
+            if (Feedbacks.Count == 3)
+            {
+                Console.WriteLine("fb no 3");
+                foreach (Feedback feedb in Feedbacks)
+                {
+                    Actions[feedb.category].Invoke(feedb);
+                }
+            }
+            Feedbacks.Clear();
+        }
+
+        public void AddAction(Category category, Action<Feedback> action, bool doOverwrite)
+        {
+            if (doOverwrite) Actions[category] += action;
+            else Actions[category] = action;
+        }
+    }
+
     internal class Program
     {
+        static void WriteToConsole(Feedback fb)
+        {
+            Console.WriteLine(
+                "No Action defined for Feedbacks of category " + fb.category
+            );
+        }
+
+
         static void Main(string[] args)
         {
             Feedback opinion1 = new Feedback(Category.OPINION, "This software is not worth the money");
@@ -21,12 +86,16 @@ namespace _2023_02_28_Events
             Feedback featreq1 = new Feedback(Category.FEATUREREQ, "Please add a way to clear the database");
             Feedback opinion2 = new Feedback(Category.OPINION, "The best solution out there");
 
-            FeedbackProcessor fproc = new FeedbackProcessor(...); // parameterkent adjunk at egy
-                                                           // default handlert ami kiirja hogy az adott
-                                                           // kategoriahoz meg nincs feedback
+            FeedbackProcessor fproc = new FeedbackProcessor(fb =>
+                Console.WriteLine(
+                    "No Action defined for Feedbacks of category " + fb.category
+                )
+            ); // parameterkent adjunk at egy
+               // default handlert ami kiirja hogy az adott
+               // kategoriahoz meg nincs feedback
 
-            fproc.AddAction(Category.BugReport, WriteToConsole, true); // 3. parameter: doOverwrite
-            fproc.AddAction(Category.FeatureRequest, WriteToConsole, true); // ha false akkor ezt az actiont IS meghivja
+            //fproc.AddAction(Category.BUGREPORT, WriteToConsole, true); // 3. parameter: doOverwrite
+            //fproc.AddAction(Category.FEATUREREQ, WriteToConsole, true); // ha false akkor ezt az actiont IS meghivja
 
             fproc.AddFeedback(opinion1);
             fproc.AddFeedback(bugreport1);
