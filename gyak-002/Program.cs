515b32c7f3a7374a20b5a9348aaaa96937ec98d2
Dr. Ádám Csapó <csapo.adam@gmail.com>
2023-03-08 10:31:49 +0100

    IEnumerable-t kiterjeszto fv console logokhoz

--- gyak-002/Program.cs
+++ gyak-002/Program.cs
@@ -3,17 +3,27 @@ using System.Xml.Linq;
 
 namespace Westeros
 {
+    static class Operations
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string header)
+        {
+            Console.WriteLine($"************* {header} ************");
+            foreach (var item in input) Console.WriteLine(item);
+            Console.WriteLine($"************* {header} ************");
+            Console.ReadLine();
+        }
+    }
+
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
             XDocument doc = XDocument.Load("02_war_of_westeros.xml");
 
             var names = doc.Descendants("name").Select(x => x.Value).ToList();
-            foreach (var name in names)
-            {
-                Console.WriteLine(name);
-            }
+            names.ToConsole("Elokeszuletek");
         }
 
     }
