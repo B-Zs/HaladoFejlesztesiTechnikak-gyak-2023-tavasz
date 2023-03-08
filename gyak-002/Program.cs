6f9973719e1b8e24dc286b0e583d1fdd85c2d336
Dr. Ádám Csapó <csapo.adam@gmail.com>
2023-03-08 10:21:57 +0100

    kezdeti lepesek

--- gyak-002/Program.cs
+++ gyak-002/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Xml.Linq;
+
+namespace Westeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XDocument doc = XDocument.Load("02_war_of_westeros.xml");
+
+            var names = doc.Descendants("name").Select(x => x.Value).ToList();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
