8d4750f80a2ba36b08abb8ede91288a412a24cc2
Adam Csapo <csapo.adam@gmail.com>
2023-03-29 11:23:00 +0200

    Create Program.cs

---
+++ gyak-004/Program.cs
@@ -0,0 +1,24 @@
+
+
+namespace Cars_Database_Application
+{
+    static class Operations
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string header)
+        {
+            Console.WriteLine($"************* {header} ************");
+            foreach (var item in input) Console.WriteLine(item);
+            Console.WriteLine($"************* {header} ************");
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarDbContext ctx = new CarDbContext();
+            ctx.Brand.Select(x => x.Name).ToConsole("BRANDS");
+        }
+    }
+}
