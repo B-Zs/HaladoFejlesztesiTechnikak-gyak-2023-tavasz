e97e2c6a86b6ec3472bededc74aec62d87ce17d0
Adam Csapo <csapo.adam@gmail.com>
2023-04-05 11:25:26 +0200

    Create CarDB.Repository-IRepository.cs

---
+++ gyak-005/CarDB.Repository-IRepository.cs
@@ -0,0 +1,11 @@
+namespace CarDB.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> ReadAll();
+        T Read(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
