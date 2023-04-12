e59b47ee3d165b87eabda26f23688b8ae52e0df7
Adam Csapo <csapo.adam@gmail.com>
2023-04-12 10:28:19 +0200

    Main fuggveny kitoltve

--- gyak-006-CarDatabase/CarDatabase/Program.cs
+++ gyak-006-CarDatabase/CarDatabase/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CarDB.Data;
+using CarDB.Models;
+using CarDB.Repository;
+
+namespace Cars_Database_Application
+{
+    static class Operations
+    {
+        public static void ToConsole<T>(this IEnumerable<T> input, string header)
+        {
+            Console.WriteLine($"************* {header} ************");
+            foreach (var item in input) Console.WriteLine(item);
+            Console.WriteLine($"************* {header} ************");
+            Console.ReadLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarDbContext ctx = new CarDbContext();
+            ctx.Brand.Select(x => x.Name).ToConsole("BRANDS");
+
+            BrandRepository brep = new BrandRepository(ctx);
+            brep.Update(new Brand() { Id = 1, Name = "XYZ" });
+
+            ctx.Brand.Select(x => x.Name).ToConsole("BRANDS");
+        }
+    }
+}
